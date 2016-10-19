2e144848319458b2e7a6d01bfc04fee334ca10db
Nate Wang <nate341124@gmail.com>
2016-10-19 11:44:15 -0700

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::
+	def self.validate_username input
+		existing_user = User.find user_name: input
+		is_admin_name = false
+		User.find 
+		if (not existing_user) or is_admin_name
+			return false
+		return true
+	end
 end
