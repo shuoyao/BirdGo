6202e10a95c708fe5b447f25c1c501348daa5f2b
Chonghui Wu <xiaowu@berkeley.edu>
2016-10-19 05:48:18 +0000

    Merging

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,2 +1,8 @@
 class ExploreController < ApplicationController
+    def index
+        
+        @search = params[:search] ? params[:search] : {}
+        #@articles = Bird.search(@search)
+        @article = Bird.find_by(name: search)
+    end
 end
