e0dcbf2b39ebce0126b5cd65f698a0db9c738daf
Shuo Yao <shuoyao@berkeley.edu>
2016-11-20 06:14:58 +0000

    Revert "add carousel for welcome page"
    This reverts commit 52dfa52b0d8f90cb809c328581a0471c4ac7856c.

--- app/assets/javascripts/test.js
+++
@@ -1,8 +0,0 @@
-    $(document).ready(function() {  
-  		 $(".carousel-inner").swiperight(function() {  
-    		  $(this).parent().carousel('prev');  
-	    		});  
-		   $(".carousel-inner").swipeleft(function() {  
-		      $(this).parent().carousel('next');  
-	   });  
-	});  
