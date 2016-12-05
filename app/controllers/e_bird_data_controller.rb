a28fe8e6a617b282048cbef60d63ba1825b2d897
Mingjian Lu <mingjian.lu@berkeley.edu>
2016-12-05 15:48:10 +0000

    Before merge

--- app/controllers/e_bird_data_controller.rb
+++ app/controllers/e_bird_data_controller.rb
@@ -3,11 +3,37 @@ require 'json'
 
 class EBirdDataController < ApplicationController
 	def NearestBirds()
+		# https://confluence.cornell.edu/display/CLOISAPI/eBird-1.1-RecentNearbyObservations
     	lng = params['lng']
     	lat = params['lat']
     	radius = params['r']
     	back_days = params['t']
     	url = 'http://ebird.org/ws1.1/data/obs/geo/recent?lng=' + lng + '&lat=' + lat + '&dist=' + radius+ '&back=' + back_days + '&fmt=json'
+    	uri = URI(url)
+		response = Net::HTTP.get(uri)
+		render json: response
+	end
+	
+	def NearestLocationsWithObservationsOfASpecies()
+		# https://confluence.cornell.edu/display/CLOISAPI/eBird-1.1-NearestLocationsWithObservationsOfASpecies
+		lng = params['lng']
+    	lat = params['lat']
+    	back_days = params['t']
+    	sci = params['sci']
+    	url = 'http://ebird.org/ws1.1/data/nearest/geo_spp/recent?lng=' + lng + '&lat=' + lat + '&back=' + back_days + '&fmt=json' + '&sci=' + sci
+    	uri = URI(url)
+		response = Net::HTTP.get(uri)
+		render json: response
+	end
+	
+	def RecentNearbyObservationsOfASpecies()
+		# https://confluence.cornell.edu/display/CLOISAPI/eBird-1.1-RecentNearbyNotableObservations
+		lng = params['lng']
+    	lat = params['lat']
+    	radius = params['r']
+    	back_days = params['t']
+    	sci = params['sci']
+    	url = 'http://ebird.org/ws1.1/data/obs/geo_spp/recent?lng' + lng + '&lat=' + lat + '&dist=' + radius+ '&back=' + back_days + '&fmt=json' + '&sci=' + sci
     	uri = URI(url)
 		response = Net::HTTP.get(uri)
 		render json: response
