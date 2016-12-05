da4cb5b953de43047db7ba7199d2b5d82803e188
Xiaoyang Bai <xiaoyang.bai@berkeley.edu>
2016-12-05 04:46:15 +0000

    www

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -1,5 +1,10 @@
 class WishlistController < ApplicationController
     def add
+        wishlist = Wishlist.find_by_id(params[:id])
+        if !current_user.wishlists.include? wishlist
+            redirect_to "/"
+            break
+        end
         bird = Bird.find_by_name(params[:name])
         wishlist = Wishlist.find_by_id(params[:id])
         wishlist.add_bird(bird)
@@ -7,8 +12,12 @@ class WishlistController < ApplicationController
     end
 
     def remove
-        bird = Bird.find_by_name(params[:name])
         wishlist = Wishlist.find_by_id(params[:id])
+        if !current_user.wishlists.include? wishlist
+            redirect_to "/"
+            break
+        end
+        bird = Bird.find_by_name(params[:name])=
         wishlist.remove_bird(bird)
         redirect_to "/"
     end
